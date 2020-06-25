0c046801c8d8c4a1c9d7d8cce2480bb08b1b3b54
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2020-06-25 17:14:54 +0200

    results brainstorming / further ideas

--- src/simulation.jl
+++ src/simulation.jl
@@ -5,17 +5,90 @@ using LightGraphs
 mutable struct Citizen <: AbstractAgent
     id::Int64
     pos::Int64
-    opinions::AbstractArray
+    opinions::Float64
     trust::Float64
-    memory::AbstractArray
+    memory::AbstractArray  # priority queue / higher weight of events frequently reported on
     newsfeed::AbstractArray
     perceived_threat::Float64
+    media_use::Float64
+    fear::Float64
+    risk_affinity::Float64
+    # frustration
     comply::Bool
 end
 
 struct Event
+    severity::Float64
+    pos::Int64
+    scope::Symbol  # :L -> local, :G -> global
+    type::String  # specify the levels somewhere
+
+    # severity
+    # proximity
+        # social proximity: event affects myself or my friends
+    # local / global
+        # somebody close gets infected
+
+    # event types:
+
+        # new policies / information
+            # travel restrictions
+            # open / close:
+                # schools / colleges / kindergartens
+                # shops / manufacturing / playgrounds / public transport / recreation facilities
+                # hospitals / physicians
+        # not adhering to policy -> consequences
+        # loosening / tightening of policies
+            # contingent on model state (e.g., number of infected -> threshold / range)
+
+        # beginning of a wave / outbreak
+            # report numbers (media)
+
+        # financial news / big company bankruptcy
+        # social topics relating to Corona
+
+        # potential remedy / vaccine
+
+        # panic / panic buying / "prepping" / irrational behavior
+        # organised protest
+
+    # analyse UK media coverage -> classify
+
 end
 
+function create_events(model)
+    # non-complicance event:
+        # subgraph of non-compliant agents
+        # find biggest component
+        # greater than threshold?
+            # find most central agent -> position of event
+            # create the event
+
+    # remedy event:
+        # if rand() < 0.01
+            # create remedy event
+
+    # outbreak event:
+        # contingent on SIR model
+end
+
+struct Newspost
+    # event
+    # type of post (video / text)
+    # accuracy
+    # credibility
+    # bias / reporting bias
+    # sentiment / emotional loading
+    # news outlet / source
+    # date of publication
+    # view count / impact
+end
+
+function share_newspost(agent, model, event)
+end
+
+
+# SIR model to be run in parallel
 
 c = Int64[
     2 2 2;
@@ -27,7 +100,11 @@ n = Int64[33, 33, 34]
 space = GraphSpace(stochastic_block_model(c, n))
 
 properties = Dict(
-    "Compliance" => 0.0
+    "compliance" => 0.0,
+    "policy_ranges" => Dict(),  # policy => range
+    "hospital_capacity" => 1.0,  # initialize with realistic number
+    "policy_restrictiveness" => 0.0,
+    "event_thresholds" => Dict()
 )
 
 model = AgentBasedModel(
@@ -53,7 +130,13 @@ for i in 1:100
     )
 end
 
-model
+function check_feed!(agent)
+    # change internal state according to feed
+end
+
+function update_memory!()
+    # update priorities
+end
 
 function agent_step!()
     # look at news feed
@@ -66,4 +149,5 @@ function agent_step!()
 end
 
 function model_step!()
+    # count number of compliant agents
 end
